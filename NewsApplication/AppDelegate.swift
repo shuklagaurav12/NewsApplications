f27b9deae9de440a6fc68005fd0777502cf01ea7
shuklagaurav12 <knitgauravshukla@gmail.com>
2017-10-12 14:21:03 +0530

    Some Changes

--- NewsApplication/AppDelegate.swift
+++ NewsApplication/AppDelegate.swift
@@ -16,6 +16,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        // something to test
+        //new
         return true
     }
 
